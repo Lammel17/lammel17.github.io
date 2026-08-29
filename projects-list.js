1fab464395b3b6bf506352b38ba123958445e5c8
Lammel17 <lukaskonstanz@web.de>
2026-08-29 22:22:29 +0200

    all corrected

--- projects-list.js
+++ projects-list.js
@@ -5,25 +5,25 @@ export const favouriteProjects = [
         category: "Game Development",
         categoryClass: "game-dev",
         description: "A minimalist Souls-like game featuring a playable Cube, focused on achieving a satisfying character movement feel. The project was built entirely from the ground up.",
-        image: "projects/Cubi/CoverImage.png",
-        page: "projects/image-example.html"
+        image: "projects/cube_souls/CoverImage.png",
+        page: "projects/cube_souls/project_page.html"
     },
 
     {
-        name: "Innovil: A Souls-Like Boss Fight (Unity)",
+        name: "Innovil: N3DS Port",
         category: "Game Development",
         categoryClass: "game-dev",
-        description: "A fully functional Souls-like boss fight where the player battles a mechanical beast. The design, models, animations, and code were all created by me.",
-        image: "projects/Innovil/CoverImage.png",
-        page: "projects/image-example.html"
+        description: "This is a passion project in which I attempted to port Innovil to the Nintendo 3DS. I was able to port the game with all of its functionality, but with reduced visual quality.",
+        image: "projects/3ds_innovil/CoverImage.png",
+        page: "projects/3ds_innovil/project_page.html"
     },
     {
         name: "Hoverbike (Unity)",
         category: "Game Development",
         categoryClass: "game-dev",
         description: "This game was built entirely from the ground up as a hoverbike simulation, where the player collects seven signs to access a gate leading seamlessly to another world.",
-        image: "projects/Hoverbike/CoverImage.png",
-        page: "projects/image-example.html"
+        image: "projects/hoverbike/CoverImage.png",
+        page: "projects/hoverbike/project_page.html"
     },
 
     {
@@ -31,8 +31,8 @@ export const favouriteProjects = [
         category: "3D Modelling",
         categoryClass: "3d",
         description: "Ester is a 3D character model made in Blender based on a concept sketch. I also experimented with rigging the model and created a short animation.",
-        image: "projects/Esteria/CoverImage.png",
-        page: "projects/image-example.html"
+        image: "projects/esteria/CoverImage.png",
+        page: "projects/esteria/project_page.html"
     },
 
     {
@@ -40,24 +40,24 @@ export const favouriteProjects = [
         category: "Coding",
         categoryClass: "code",
         description: "An unfinished mini-platformer about a small blue alien, made in Java using the Processing application. I created the game physics code and pixel art myself.",
-        image: "projects/deep-down/CoverImage.png",
-        page: "projects/image-example.html"
+        image: "projects/deep_down/CoverImage.png",
+        page: "projects/deep_down/project_page.html"
     },
     {
         name: "Lucky Tower Ultimate (Internship)",
         category: "Game Development Internship",
         categoryClass: "none",
         description: "During my practical semester, I worked for six months at Studio Seufz in Stuttgart, Germany. The experience gave me a lot of insight into the game development industry.",
-        image: "projects/Lucky-Tower/CoverImage.png",
-        page: "projects/image-example.html"
+        image: "projects/lucky_tower/CoverImage.png",
+        page: "projects/lucky_tower/project_page.html"
     },
     {
         name: "Showreel",
         category: "Animation",
         categoryClass: "animation",
         description: "This is my showreel, a collection of video clips from various projects.",
-        image: "projects/Showreel/CoverImage.png",
-        page: "projects/image-example.html"
+        image: "projects/showreel/CoverImage.png",
+        page: "projects/showreel/project_page.html"
     },
 
     {
@@ -65,8 +65,8 @@ export const favouriteProjects = [
         category: "Game Development",
         categoryClass: "game-dev",
         description: "This game was a team project in which we created a 2D escape platformer from the ground up. It is about a mad jester trapped in a dungeon.",
-        image: "projects/Amentior/CoverImage.png",
-        page: "projects/image-example.html"
+        image: "projects/amentior/CoverImage.png",
+        page: "projects/amentior/project_page.html"
     },
 
     {
@@ -74,8 +74,8 @@ export const favouriteProjects = [
         category: "Research",
         categoryClass: "none",
         description: "For my bachelor's thesis, I wrote about the use cases of Root Motion. I analyzed the effects of its use from both a system and player perspective.",
-        image: "projects/Bachelor-thesis/CoverImage.png",
-        page: "projects/image-example.html"
+        image: "projects/bachelor_thesis/CoverImage.png",
+        page: "projects/bachelor_thesis/project_page.html"
     },
 
     {
@@ -83,8 +83,8 @@ export const favouriteProjects = [
         category: "3D Modeling",
         categoryClass: "3d",
         description: "Here, I designed a character and then modeled him in Blender. I sculpted the character first and then retopologized the model while striving for a realistic look.",
-        image: "projects/Coastguard-henry/CoverImage.png",
-        page: "projects/image-example.html"
+        image: "projects/coast_guard/CoverImage.png",
+        page: "projects/coast_guard/project_page.html"
     },
 
     {
@@ -92,16 +92,16 @@ export const favouriteProjects = [
         category: "Coding",
         categoryClass: "code",
         description: "To place the feet and correctly angle the knees on uneven terrain in games, I created a script as my own attempt to solve this problem.",
-        image: "projects/Footing-code/CoverImage.png",
-        page: "projects/image-example.html"
+        image: "projects/foot_placement_code/CoverImage.png",
+        page: "projects/foot_placement_code/project_page.html"
     },
     {
         name: "Lend A Hand",
         category: "Animation",
         categoryClass: "animation",
         description: "A hand-drawn, frame-by-frame animation of a green hand annoyed by a fly. The hand and fly get into an interesting fight before eventually reconciling.",
-        image: "projects/hand-animation/CoverImage.png",
-        page: "projects/image-example.html"
+        image: "projects/lend_a_hand/CoverImage.png",
+        page: "projects/lend_a_hand/project_page.html"
     },
 ];
 
@@ -115,8 +115,8 @@ export const allProjects = [
         category: "Game Development",
         categoryClass: "game-dev",
         description: "A minimalist Souls-like game featuring a playable Cube, focused on achieving a satisfying character movement feel. The project was built entirely from the ground up.",
-        image: "projects/Cubi/CoverImage.png",
-        page: "projects/image-example.html"
+        image: "projects/cube_souls/CoverImage.png",
+        page: "projects/cube_souls/project_page.html"
     },
 
     {
@@ -124,8 +124,8 @@ export const allProjects = [
         category: "Coding",
         categoryClass: "code",
         description: "There is no better way to display a collection of projects I have worked on than through my own portfolio site. Through it, I refreshed my knowledge of HTML, CSS, and JavaScript.",
-        image: "projects/Portfolio-site/CoverImage.png",
-        page: "projects/image-example.html"
+        image: "projects/portfolio_site/CoverImage.png",
+        page: "projects/portfolio_site/project_page.html"
     },
 
     {
@@ -133,8 +133,8 @@ export const allProjects = [
         category: "Research",
         categoryClass: "none",
         description: "For my bachelor's thesis, I wrote about the use cases of Root Motion. I analyzed the effects of its use from both a system and player perspective.",
-        image: "projects/Bachelor-thesis/CoverImage.png",
-        page: "projects/image-example.html"
+        image: "projects/bachelor_thesis/CoverImage.png",
+        page: "projects/bachelor_thesis/project_page.html"
     },
 
 
@@ -143,8 +143,8 @@ export const allProjects = [
         category: "Game Development",
         categoryClass: "game-dev",
         description: "This is a passion project in which I attempted to port Innovil to the Nintendo 3DS. I was able to port the game with all of its functionality, but with reduced visual quality.",
-        image: "projects/3ds-Innovil/CoverImage.png",
-        page: "projects/image-example.html"
+        image: "projects/3ds_innovil/CoverImage.png",
+        page: "projects/3ds_innovil/project_page.html"
     },
 
     {
@@ -152,8 +152,8 @@ export const allProjects = [
         category: "Game Development Internship",
         categoryClass: "none",
         description: "During my practical semester, I worked for six months at Studio Seufz in Stuttgart, Germany. The experience gave me a lot of insight into the game development industry.",
-        image: "projects/Lucky-Tower/CoverImage.png",
-        page: "projects/image-example.html"
+        image: "projects/lucky_tower/CoverImage.png",
+        page: "projects/lucky_tower/project_page.html"
     },
 
     {
@@ -161,8 +161,8 @@ export const allProjects = [
         category: "Digital Art",
         categoryClass: "artwork",
         description: "These weapons are a homage to the game Bloodborne, which has an amazing collection of weapons. This is my own attempt to expand that collection.",
-        image: "projects/Weapon-Collection/CoverImage.png",
-        page: "projects/image-example.html"
+        image: "projects/weapon_collection/CoverImage.png",
+        page: "projects/weapon_collection/project_page.html"
     },
 
     {
@@ -170,8 +170,8 @@ export const allProjects = [
         category: "Coding",
         categoryClass: "code",
         description: "To place the feet and correctly angle the knees on uneven terrain in games, I created a script as my own attempt to solve this problem.",
-        image: "projects/Footing-code/CoverImage.png",
-        page: "projects/image-example.html"
+        image: "projects/foot_placement_code/CoverImage.png",
+        page: "projects/foot_placement_code/project_page.html"
     },
 
     {
@@ -179,8 +179,8 @@ export const allProjects = [
         category: "Game Development",
         categoryClass: "game-dev",
         description: "A fully functional Souls-like boss fight where the player battles a mechanical beast. The design, models, animations, and code were all created by me.",
-        image: "projects/Innovil/CoverImage.png",
-        page: "projects/image-example.html"
+        image: "projects/innovil/CoverImage.png",
+        page: "projects/innovil/project_page.html"
     },
 
     {
@@ -188,8 +188,8 @@ export const allProjects = [
         category: "Animation",
         categoryClass: "animation",
         description: "This is my showreel, a collection of video clips from various projects.",
-        image: "projects/Showreel/CoverImage.png",
-        page: "projects/image-example.html"
+        image: "projects/showreel/CoverImage.png",
+        page: "projects/showreel/project_page.html"
     },
 
     {
@@ -197,8 +197,8 @@ export const allProjects = [
         category: "3D Modelling",
         categoryClass: "3d",
         description: "Ester is a 3D character model made in Blender based on a concept sketch. I also experimented with rigging the model and created a short animation.",
-        image: "projects/Esteria/CoverImage.png",
-        page: "projects/image-example.html"
+        image: "projects/esteria/CoverImage.png",
+        page: "projects/esteria/project_page.html"
     },
 
     {
@@ -206,8 +206,8 @@ export const allProjects = [
         category: "Game Development",
         categoryClass: "game-dev",
         description: "This game was built entirely from the ground up as a hoverbike simulation, where the player collects seven signs to access a gate leading seamlessly to another world.",
-        image: "projects/Hoverbike/CoverImage.png",
-        page: "projects/image-example.html"
+        image: "projects/hoverbike/CoverImage.png",
+        page: "projects/hoverbike/project_page.html"
     },
 
     {
@@ -215,8 +215,8 @@ export const allProjects = [
         category: "Game Development",
         categoryClass: "game-dev",
         description: "This short sci-fi VR experience is a puzzle game in which the player has to navigate through an old factory and find their way to the other side.",
-        image: "projects/VR-puzzle/CoverImage.png",
-        page: "projects/image-example.html"
+        image: "projects/node_vr_puzzle/CoverImage.png",
+        page: "projects/node_vr_puzzle/project_page.html"
     },
 
     {
@@ -224,8 +224,8 @@ export const allProjects = [
         category: "Film-Audio",
         categoryClass: "film-audio",
         description: "With this project, I challenged myself to give a Dark Souls cinematic a new sound design. It was particularly interesting as a person with hearing impairment.",
-        image: "projects/audio-design/CoverImage.png",
-        page: "projects/image-example.html"
+        image: "projects/audio_design_challenge/CoverImage.png",
+        page: "projects/audio_design_challenge/project_page.html"
     },
 
     {
@@ -233,8 +233,8 @@ export const allProjects = [
         category: "Game Development",
         categoryClass: "game-dev",
         description: "This game was a team project in which we created a 2D escape platformer from the ground up. It is about a mad jester trapped in a dungeon.",
-        image: "projects/Amentior/CoverImage.png",
-        page: "projects/image-example.html"
+        image: "projects/amentior/CoverImage.png",
+        page: "projects/amentior/project_page.html"
     },
 
     {
@@ -242,8 +242,8 @@ export const allProjects = [
         category: "Card Game",
         categoryClass: "none",
         description: "Memory Snake is a small and simple card game in which one player guesses the shape of a snake and has to remember the correct tiles.",
-        image: "projects/card-game/CoverImage.png",
-        page: "projects/image-example.html"
+        image: "projects/memory_snake/CoverImage.png",
+        page: "projects/memory_snake/project_page.html"
     },
 
     {
@@ -251,8 +251,8 @@ export const allProjects = [
         category: "Film-Audio",
         categoryClass: "film-audio",
         description: "In this short film, I worked in a team to present a theoretical photovoltaic canopy for a car park. The goal was to market the concept.",
-        image: "projects/visual-storytelling/CoverImage.png",
-        page: "projects/visual-storytelling/photovoltaic-canopy.html"
+        image: "projects/photovoltaic_canopy/CoverImage.png",
+        page: "projects/photovoltaic_canopy/project_page.html"
     },
 
     {
@@ -260,8 +260,8 @@ export const allProjects = [
         category: "Coding",
         categoryClass: "code",
         description: "An unfinished mini-platformer about a small blue alien, made in Java using the Processing application. I created the game physics code and pixel art myself.",
-        image: "projects/deep-down/CoverImage.png",
-        page: "projects/deep-down/deep_down_aliens.html"
+        image: "projects/deep_down/CoverImage.png",
+        page: "projects/deep_down/project_page.html"
     },
 
     {
@@ -269,8 +269,8 @@ export const allProjects = [
         category: "3D Modeling",
         categoryClass: "3d",
         description: "Here, I designed a character and then modeled him in Blender. I sculpted the character first and then retopologized the model while striving for a realistic look.",
-        image: "projects/Coastguard-henry/CoverImage.png",
-        page: "projects/Coastguard-henry/coastguard.html"
+        image: "projects/coast_guard/CoverImage.png",
+        page: "projects/coast_guard/project_page.html"
     },
 
     {
@@ -278,8 +278,8 @@ export const allProjects = [
         category: "Animation",
         categoryClass: "animation",
         description: "A hand-drawn, frame-by-frame animation of a green hand annoyed by a fly. The hand and fly get into an interesting fight before eventually reconciling.",
-        image: "projects/hand-animation/CoverImage.png",
-        page: "projects/hand-animation/hand_animation.html"
+        image: "projects/lend_a_hand/CoverImage.png",
+        page: "projects/lend_a_hand/project_page.html"
     },
 
     {
@@ -287,8 +287,8 @@ export const allProjects = [
         category: "Film-Audio",
         categoryClass: "film-audio",
         description: "This short film explores the depressing pressure created by the feeling of monotony. I worked in a small team, where I helped with shooting and editing the film.",
-        image: "projects/Monotonie/CoverImage.png",
-        page: "projects/Monotonie/monotone_film.html"
+        image: "projects/monotony/CoverImage.png",
+        page: "projects/monotony/project_page.html"
     },
 
     {
@@ -296,8 +296,8 @@ export const allProjects = [
         category: "Coding",
         categoryClass: "code",
         description: "When learning to code, it is useful to create multiple small projects that cover different concepts individually.",
-        image: "projects/Java-Coding/CoverImage.png",
-        page: "projects/Java-Coding/Java_Coding_Excercise.html"
+        image: "projects/java_excercise/CoverImage.png",
+        page: "projects/java_excercise/project_page.html"
     },
 
     {
@@ -305,8 +305,8 @@ export const allProjects = [
         category: "Digital Artworks",
         categoryClass: "artwork",
         description: "Nite Prime (NPC) has been my artist name since school. I have been a digital artist for most of my life, which inspired me to study Media Design at RWU.",
-        image: "projects/Digital-art/CoverImage.png",
-        page: "projects/Digital-art/Nite-Prime-Creations.html"
+        image: "projects/nite_prime/CoverImage.png",
+        page: "projects/nite_prime/project_page.html"
     },
 
 ];
